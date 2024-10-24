4de702d074bfdee0793df63bb00cc5b0e0d8d456
Miguel Angel Vigo Baz <mvigo@AQUATICA.LOCAL>
2024-10-24 17:09:24 +0200

    Pagina de Crear Usuario sin funcionalidad

--- newaccount.php
+++ newaccount.php
@@ -1,10 +1,11 @@
+
 <!DOCTYPE html>
 <html lang="es">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>App Nasa</title>
+    <title>Create User</title>
     <link rel="stylesheet" href="css\styles.css">
     <link rel="shortcut icon" href="assets\favicon.ico" type="image/x-icon">
 
@@ -19,14 +20,18 @@
         <div class="form">
             <form action="" method="post">
                 <label for="name">Usuario</label>
-                <input type="text" name="name" id="name" placeholder="Name">
+                <input type="text" name="name" id="name" placeholder="Name" required>
                 <label for="password">Password</label>
-                <input type="password" name="password" id="password" placeholder="Password">
-                <button type="submit"> Login </button>
+                <input type="password" name="password" id="password" placeholder="Password" required>
+                <label for="conpassword">Confirm Password</label>
+                <input type="password" name="conpassword" id="conpassword" placeholder="Confirm Password" required>
+                <label for="token">Token</label>
+                <input type="text" name="token" id="token" placeholder="Token">
+                <button type="submit"> Create User </button>
             </form>
         </div>
-        <div class="newAccount">
-            <a href="newaccount.php">Create a new Account</a>
+        <div class="Login">
+            <a href="login.php">Login</a>
         </div>
 
     </div>
