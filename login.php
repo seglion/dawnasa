512737734376e0e6c4dfc381a89e60addeebefa5
Miguel Angel Vigo Baz <mvigo@AQUATICA.LOCAL>
2024-10-24 16:34:02 +0200

    Pagina de Login sin funcionalidad

---
+++ login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>App Nasa</title>
+    <link rel="stylesheet" href="css\styles.css">
+    <link rel="shortcut icon" href="assets\favicon.ico" type="image/x-icon">
+
+
+</head>
+
+<body>
+    <div class="container">
+        <div class="logo">
+            <img id='logo' src="assets\Nasa_logo.svg" alt="Logo Nasa">
+        </div>
+        <div class="form">
+            <form action="" method="post">
+                <label for="name">Usuario</label>
+                <input type="text" name="name" id="name" placeholder="Name">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" placeholder="Password">
+                <button type="submit"> Login </button>
+            </form>
+        </div>
+        <div class="newAccount">
+            <a href="">Create a new Account</a>
+        </div>
+
+
+
+    </div>
+
+</body>
+
+</html>
